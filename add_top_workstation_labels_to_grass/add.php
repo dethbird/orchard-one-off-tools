835cc8c00cf4ee6dd706f8a006e680d6a7ce3bcf
Rishi Satsangi <rishi.satsangi@gmail.com>
2015-06-04 15:07:17 -0400

    take multiple successive .csv files as arguments

--- add_top_workstation_labels_to_grass/add.php
+++ add_top_workstation_labels_to_grass/add.php
@@ -1,5 +1,5 @@
 <?php   
-
+// Get all the .csv exports for 5000 per page from Google Analytics https://goo.gl/J4XFGX
 require 'vendor/autoload.php';
 
 use Symfony\Component\Yaml\Yaml;
@@ -9,21 +9,36 @@ $yml = Yaml::parse(file_get_contents('vendor/orchard/features/configs/features.y
 $orig = $yml['grass_analytics']['enabled']['users'];
 echo "current label count: " . count($orig) . "\n";
 
-// location of .csv exported from Google Analytics for top analytics pageview labels
-echo "adding next top " . $argv[1] . " label(s) from:" . $argv[2] ."\n";
-$top_labels_spreadsheet = file_get_contents($argv[2]);
-$top_labels_spreadsheet = explode("\n", $top_labels_spreadsheet);
+// location of .csv files in the next args exported from Google Analytics for top analytics pageview labels
+$i = 2;
+$spreadsheet_files = array();
+while(isset($argv[$i])) {
+    $spreadsheet_files[] = $argv[$i];
+    $i++;
+}
+
+echo "adding next top " . $argv[1] . " label(s) from:\n" . implode(",\n", $spreadsheet_files) ."\n";
+
 
 // extract top labels
 $top_labels = array();
-foreach ($top_labels_spreadsheet as $line) {
-   if( stripos($line, "/analytics/index")===0 ) {
-      $line = explode(",", $line);
-      $top_labels[] = $line[1];
-   }
-} 
 
-echo "labels found from .csv: " . count($top_labels) . "\n";
+foreach ($spreadsheet_files as $file) {
+  $top_labels_spreadsheet = file_get_contents($argv[2]);
+  $top_labels_spreadsheet = explode("\n", $top_labels_spreadsheet);
+
+  foreach ($top_labels_spreadsheet as $line) {
+     if( stripos($line, "/analytics/overview")===0 ) {
+        $line = explode(",", $line);
+        $top_labels[] = $line[1];
+     }
+  } 
+
+}
+
+$top_labels = array_unique($top_labels);
+
+echo "unique labels found from .csv: " . count($top_labels) . "\n";
 
 // add only if they don't already exist in features.yml
 $count = 0;
@@ -71,14 +86,9 @@ while ($row = mysql_fetch_object($res)) {
     // echo $row->name."\n";
 }
 
-
-
-
-
 // echo $output;
 file_put_contents("grass.feature.labels.yml", $output);
 file_put_contents("grass.feature.labels.tab", $outputTabbed);
-
 
 // get a handful to test with that don't have grass
 $query = "SELECT vendor_id, IF(name,name,company) as name, priority, overall_priority, label_identifier FROM vendor WHERE vendor_id NOT IN (" . implode(",", $new) . ") AND priority < 3 ORDER BY RAND() LIMIT 250";
@@ -97,8 +107,6 @@ while ($row = mysql_fetch_object($res)) {
     $outputTabbed .= $row->vendor_id . "\t" . $row->name. "\t" . $row->priority. "\t" . $row->overall_priority. "\t" . $row->label_identifier . "\n";
     // echo $row->name."\n";
 }
-
-
 
 file_put_contents("grass.feature.labels.off.tab", $outputTabbed);
 
